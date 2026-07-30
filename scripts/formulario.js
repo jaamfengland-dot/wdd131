4528648c85d320c65b26fafb2d5f2ee7f2a8bcec
jaamfengland <jferreira5@byupatway.edu>
2026-07-30 16:22:01 -0300

    novo

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -6,16 +6,35 @@ document.getElementById("anoatual").textContent = anoAtual;
 document.getElementById("ultimamodificacao").textContent =
     `última modificação: ${document.lastModified}`;
 
-// Array de produtos fornecido pela atividade
+// Array de produtos 
 const produtos = [
-  { id: "fc-1888", nome: "capacitor de fluxo", classificacaomedia: 4.5 },
-  { id: "fc-2050", nome: "fios elétricos", classificacaomedia: 4.7 },
-  { id: "fs-1987", nome: "circuitos de tempo", classificacaomedia: 3.5 },
-  { id: "ac-2000", nome: "reator de baixa tensão", classificacaomedia: 3.9 },
-  { id: "jj-1969", nome: "equalizador de distorção", classificacaomedia: 5.0 }
-];
-
-// Preenche o select com as opções dinamicamente
+  {
+    id: "fc-1888",
+    nome: "capacitor de fluxo",
+    classificacaomedia: 4.5
+  },
+  {
+    id: "fc-2050",
+    nome: "fios elétricos",
+    classificacaomedia: 4.7
+  },
+  {
+    id: "fs-1987",
+    nome: "circuitos de tempo",
+    classificacaomedia: 3.5
+  },
+  {
+    id: "ac-2000",
+    nome: "reator de baixa tensão",
+    classificacaomedia: 3.9
+  },
+  {
+    id: "jj-1969",
+    nome: "equalizador de distorção",
+    classificacaomedia: 5.0
+  }
+  ];
+// Preenche o select 
 const selectProduto = document.getElementById("produto");
 
 produtos.forEach((produto) => {
