f9a810d1d21426fcedd24aaf83f7028c0e8e65d3
jaamfengland <jferreira5@byupatway.edu>
2026-08-05 12:24:12 -0300

    projeto atualizado

--- Projeto/script/obterdatas.js
+++ Projeto/script/obterdatas.js
@@ -1,9 +1,29 @@
+// RODAPÉ (Ano e Última Modificação)
+
 // Obtém o ano atual
 const anoAtual = new Date().getFullYear();
 
-// Exibe o ano dos direitos autorais
-document.getElementById("anoatual").textContent = anoAtual;
+// Exibe o ano dos direitos autorais se o elemento existir
+const elemAno = document.getElementById("anoatual");
+if (elemAno) {
+    elemAno.textContent = anoAtual;
+}
 
-// Exibe a data da última modificação do documento
-document.getElementById("ultimamodificacao").textContent =
-    `ultima modificação: ${document.lastModified}`;
+// Exibe a data da última modificação do documento se o elemento existir
+const elemModif = document.getElementById("ultimamodificacao");
+if (elemModif) {
+    elemModif.textContent = `última modificação: ${document.lastModified}`;
+}
+
+// FORMULÁRIO (Salva contagem no localStorage)
+
+const formHistoria = document.getElementById("form-historia");
+
+if (formHistoria) {
+    formHistoria.addEventListener("submit", () => {
+        // Incrementa o contador de envios no localStorage
+        let avaliacoesEnviadas = Number(window.localStorage.getItem("avaliacoes-count")) || 0;
+        avaliacoesEnviadas++;
+        window.localStorage.setItem("avaliacoes-count", avaliacoesEnviadas);
+    });
+}
